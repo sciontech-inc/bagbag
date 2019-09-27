2dd16b52ce66eed7c7f1f6c15d5233576c9904a2
Jetro <jetromacalipay@gmail.com>
2019-09-28 05:42:40 +0800

    fsd

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -38,7 +38,25 @@ class TransactionController extends Controller
 
         $blotterRecord = Blotter::where('deleted_at', NULL)->orderBy('id')->get(); 
         if (Blotter::where('suspect', $residentName)->exists()) {
-            return redirect()->back()->with('success','Warning! Cannot proceed because Resident have blotter Record.');
+            $test = Blotter::select('deleted_at')->where('suspect', $residentName)->first();
+            if( $test->deleted_at === NULL) {
+                return redirect()->back()->with('success','Warning! Cannot proceed because Resident have blotter Record.');
+            } else {
+                $image = $request->file('image');
+                $new_name = rand() . '.' . $image->getClientOriginalExtension();
+                $image->move(public_path('images'), $new_name);
+        
+                $clearance = new Certification([
+                    'resident_id' => $request->resident_id,
+                    'purpose' => $request->purpose,
+                    'image' => $new_name,
+                ]);
+                $clearance->save();
+        
+                $residents = Resident::orderBy('id')->get();
+                $certifications = Certification::with('resident')->where('id', $clearance->id)->get();
+                return view('businessDev.pages.transaction.clearance', compact('residents', 'certifications'));
+            }
         } else {
             $image = $request->file('image');
             $new_name = rand() . '.' . $image->getClientOriginalExtension();
